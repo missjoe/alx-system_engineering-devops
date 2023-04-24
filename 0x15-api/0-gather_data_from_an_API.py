2460bfb020d3f6120d8ee800ac6200294b049fd0
John Tochi Josephine <johntochijosephine1995@gmail.com>
2023-04-24 07:51:34 -0700

    Task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,24 @@
+#!/user/bin/python3
+"""
+Gather data from an API
+"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    user_id = argv[1]
+    base_url = "https://jsonplaceholder.typicode.com/"
+    employee = requests.get(base_url + "users/{}".format(user_id)).json()
+    tasks = requests.get(base_url + "todos", params={"userId": user_id}).json()
+
+    completed_tasks = []
+    for task in tasks:
+        if task.get("completed"):
+            completed_tasks.append(task.get("title"))
+
+print("Employee {} is done with tasks({}/{}):".format(
+    employee.get("name"), len(completed_tasks), len(tasks)))
+
+for task in completed_tasks:
+    print("\t {}".format(task))
