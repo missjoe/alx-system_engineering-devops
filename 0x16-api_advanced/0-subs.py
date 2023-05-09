5f03fb99b593f98aeb889f7b7d6d26aa322ba51d
John Tochi Josephine <johntochijosephine1995@gmail.com>
2023-05-09 08:55:51 -0700

    Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,14 @@
 import requests
 
+
 def number_of_subscribers(subreddit):
     # Set custom User-Agent header to avoid Too Many Requests error
     headers = {'User-Agent': 'my_bot/0.0.1'}
-    
+
     # Send GET request to Reddit API
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
+
     # Check if subreddit exists
     if response.status_code == 200:
         data = response.json()
