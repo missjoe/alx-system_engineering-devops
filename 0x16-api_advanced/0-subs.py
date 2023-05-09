76e9f0e1fa8eac08c08506f368f9353d7e8971e1
John Tochi Josephine <johntochijosephine1995@gmail.com>
2023-05-09 09:34:18 -0700

    Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,25 @@
+#!/usr/bin/python3
+"""
+Queries the Reddit API and returns the number of
+subscribers for a given subreddit
+
+"""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    # Set custom User-Agent header to avoid Too Many Requests error
-    headers = {'User-Agent': 'my_bot/0.0.1'}
-
-    # Send GET request to Reddit API
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    """
+    Returns the number of subscribers for a given subreddit
+    """
+    headers = {
+        'User-Agent': 'Mozilla/5.0'
+    }
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     response = requests.get(url, headers=headers, allow_redirects=False)
 
-    # Check if subreddit exists
     if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
-        return 0
+        data = response.json().get('data')
+        if data:
+            return data.get('subscribers', 0)
+    return 0
