9883c42c8cff0ed5ec6c01f7e9909109a3f8f224
John Tochi Josephine <johntochijosephine1995@gmail.com>
2023-05-10 07:05:01 -0700

    Task 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,20 +1,6 @@
-class webserver {
-  package { 'apache2':
-    ensure => installed,
-  }
-
-  file { '/etc/apache2/ports.conf':
-    ensure => file,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0644',
-    content => "Listen 8080\n",
-    require => Package['apache2'],
-  }
-
-  service { 'apache2':
-    ensure  => running,
-    enable  => true,
-    require => File['/etc/apache2/ports.conf'],
-  }
+# fixes a file
+exec {'replaces wrong php filetype':
+  command => 'sed -i "s/.phpp/.php/g" /var/www/html/wp-settings.php',
+  path    => '/bin/:/sbin/:/usr/bin/:/usr/sbin/',
+  onlyif  => 'test -f /var/www/html/wp-settings.php'
 }
